6911b6cabfdf93f297caee53419618959c356a95
Ole Kristian Losvik <losvik@gmail.com>
2021-04-13 21:46:04 +0200

    Removed abstract notificationhubitnerface

--- src/Spark.Web/Hubs/MaintenanceHub.cs
+++ src/Spark.Web/Hubs/MaintenanceHub.cs
@@ -76,57 +76,44 @@ namespace Spark.Web.Hubs
       return list;
     }
 
-    private ImportProgressMessage Message(string message, int idx)
+    public async Task ClearStore()
     {
-      var msg = new ImportProgressMessage
-      {
-        Message = message,
-        Progress = (int)10 + (idx + 1) * 90 / _resourceCount
-      };
-      return msg;
-    }
-
-    public async void ClearStore()
-    {
-      var notifier = new HubContextProgressNotifier(_hubContext, _logger);
       try
       {
-        await notifier.SendProgressUpdate("Clearing the database...", 0);
+        await Clients.All.SendAsync("UpdateProgress", "Starting clearing database...");
         _fhirStoreAdministration.Clean();
         _fhirIndex.Clean();
-        await notifier.SendProgressUpdate("Database cleared", 100);
+        await Clients.All.SendAsync("UpdateProgress", "Database cleared");
       }
       catch (Exception e)
       {
-        await notifier.SendProgressUpdate("ERROR CLEARING :( " + e.InnerException, 100);
+        await Clients.All.SendAsync("UpdateProgress", "ERROR CLEARING :( " + e.InnerException.Message);
       }
 
     }
 
-    public async void RebuildIndex()
+    public async Task RebuildIndex()
     {
-      var notifier = new HubContextProgressNotifier(_hubContext, _logger);
       try
       {
-        await _indexRebuildService.RebuildIndexAsync(notifier)
+        await Clients.All.SendAsync("UpdateProgress", "Rebuilding index...");
+        await _indexRebuildService.RebuildIndexAsync()
             .ConfigureAwait(false);
       }
       catch (Exception e)
       {
         _logger.LogError(e, "Failed to rebuild index");
 
-        await notifier.SendProgressUpdate("ERROR REBUILDING INDEX :( " + e.InnerException, 100)
+        await Clients.All.SendAsync("UpdateProgress", "ERROR REBUILDING INDEX :( " + e.InnerException.Message)
             .ConfigureAwait(false);
       }
     }
 
-    public async void LoadExamplesToStore()
+    public async Task LoadExamplesToStore()
     {
-      var messages = new StringBuilder();
-      var notifier = new HubContextProgressNotifier(_hubContext, _logger);
       try
       {
-        await notifier.SendProgressUpdate("Loading examples data...", 1);
+        await Clients.All.SendAsync("UpdateProgress", "Loading examples");
         _resources = GetExampleData();
 
         var resarray = _resources.ToArray();
@@ -136,8 +123,8 @@ namespace Spark.Web.Hubs
         {
           var res = resarray[x];
           // Sending message:
-          var msg = Message("Importing " + res.ResourceType.ToString() + " " + res.Id + "...", x);
-          await notifier.SendProgressUpdate(msg.Message, msg.Progress);
+          var msg = $"Importing {res.ResourceType.ToString()} {res.Id} ...";
+          await Clients.All.SendAsync("UpdateProgress", msg);
 
           try
           {
@@ -154,80 +141,15 @@ namespace Spark.Web.Hubs
           }
           catch (Exception e)
           {
-            // Sending message:
-            var msgError = Message("ERROR Importing " + res.ResourceType.ToString() + " " + res.Id + "... ", x);
-            await Clients.All.SendAsync("Error", msg);
-            messages.AppendLine(msgError.Message + ": " + e.Message);
+            var msgError = $"ERROR Importing {res.ResourceType.ToString()} {res.Id}... {e.InnerException.Message}";
+            await Clients.All.SendAsync("UpdateProgress", msgError);
           }
-
-
         }
-
-        await notifier.SendProgressUpdate(messages.ToString(), 100);
       }
       catch (Exception e)
       {
-        await notifier.Progress("Error: " + e.Message);
+        await Clients.All.SendAsync("UpdateProgress", "Error: " + e.Message);
       }
     }
-  }
-
-  /// <summary>
-  /// SignalR hub is a short-living object while
-  /// hub context lives longer and can be used for
-  /// accessing Clients collection between requests.
-  /// </summary>
-  internal class HubContextProgressNotifier : IIndexBuildProgressReporter
-  {
-    private readonly IHubContext<MaintenanceHub> _hubContext;
-    private readonly ILogger<MaintenanceHub> _logger;
-
-    private int _progress;
-
-    public HubContextProgressNotifier(
-        IHubContext<MaintenanceHub> hubContext,
-        ILogger<MaintenanceHub> logger)
-    {
-      _hubContext = hubContext;
-      _logger = logger;
-    }
-
-    public async Task SendProgressUpdate(string message, int progress)
-    {
-      _logger.LogInformation($"[{progress}%] {message}");
-
-      _progress = progress;
-
-      var msg = new ImportProgressMessage
-      {
-        Message = message,
-        Progress = progress
-      };
-
-      await _hubContext.Clients.All.SendAsync("UpdateProgress", msg);
-    }
-
-    public async Task Progress(string message)
-    {
-      await SendProgressUpdate(message, _progress);
-    }
-
-    public async Task ReportProgressAsync(int progress, string message)
-    {
-      await SendProgressUpdate(message, progress)
-          .ConfigureAwait(false);
-    }
-
-    public async Task ReportErrorAsync(string message)
-    {
-      await Progress(message)
-          .ConfigureAwait(false);
-    }
-  }
-
-  internal class ImportProgressMessage
-  {
-    public int Progress;
-    public string Message;
   }
 }
